7988e7c8153cae2422401f73e052702e1f3817b0
thePmSquare <impmmmm@gmail.com>
2020-07-13 21:14:03 +0530

    initial setup for lights out game

---
+++ src/LightsOut.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./stylesheets/LightsOut.css";
+
+class LightsOut extends Component {
+  render() {
+    return (
+      <div className="LightsOut">
+        <h1>Lights Out</h1>
+      </div>
+    );
+  }
+}
+
+export default LightsOut;
