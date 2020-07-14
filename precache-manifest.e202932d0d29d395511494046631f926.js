13ec77ce18d900bd43d252577e1e5a3a096ce892
thePmSquare <impmmmm@gmail.com>
2020-07-14 05:31:39 +0530

    Updates

---
+++ precache-manifest.e202932d0d29d395511494046631f926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9e6f720757864f2917233fe798d304",
+    "url": "/lights-out-game-react/index.html"
+  },
+  {
+    "revision": "cc37f989c2a2ff2da67c",
+    "url": "/lights-out-game-react/static/css/main.2090253e.chunk.css"
+  },
+  {
+    "revision": "bfc9bc06d4d26a8b8603",
+    "url": "/lights-out-game-react/static/js/2.176e5c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out-game-react/static/js/2.176e5c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc37f989c2a2ff2da67c",
+    "url": "/lights-out-game-react/static/js/main.c845a682.chunk.js"
+  },
+  {
+    "revision": "5595811b842116441fdc",
+    "url": "/lights-out-game-react/static/js/runtime-main.542bccdc.js"
+  },
+  {
+    "revision": "f5e7b513f5ec5cb41b90d378369908a8",
+    "url": "/lights-out-game-react/static/media/Typo_Round_Regular_Demo.f5e7b513.otf"
+  }
+]);
